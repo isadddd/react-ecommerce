8604abc1c0b26661122e1b73a56ee7c493bd4e7a
Irsyad MI <irsyad@madeindonesia.com>
2026-09-14 11:00:11 +0700

    update: add routing

---
+++ src/Components/Elements/Button/index.jsx
@@ -0,0 +1,14 @@
+const Button = ({ children, variant = 'primary' }) => {
+  const variants = {
+    primary: 'bg-black text-white hover:bg-gray-800',
+    secondary: 'bg-gray-100 text-black hover:bg-gray-200',
+  }
+
+  return (
+    <button className={`rounded-lg px-5 py-3 ${variants[variant]}`}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
---
+++ src/Components/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import Button from '@/Components/Elements/Button'
+const Home = () => {
+  return (
+    <section className="w-full px-4">
+    <div className="mx-auto w-full max-w-295 bg-[red]">
+      <Button>asd123</Button>
+    </div>
+  </section>
+  )
+};
+
+export default Home;
